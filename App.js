95d9c2bee2c20236a911923c0291b28982295999
Shreea Sri <shreeasri@gmail.com>
2023-09-16 23:15:18 +0530

    used props for dynamic data

--- App.js
+++ App.js
@@ -26,140 +26,108 @@ const styleCard = {
   backgroundColor: "#f0f0f0",
 };
 
+const resList = [
+  {
+    id: 1,
+    name: "KFC",
+    cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+    costForTwo: 40000,
+    costForTwoString: "₹400 FOR TWO",
+    cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+    deliveryTime: 36,
+    avgRating: 4.4,
+    minDeliveryTime: 36,
+    maxDeliveryTime: 36,
+    deliveryTime: "36 MINS",
+    lastMileTravel: 3.5,
+    address: "KFC restaurants, 942,SV Tower, 16th Main, BTM 2nd Stage ,560076",
+    locality: "2nd Stage",
+    parentId: 547,
+    unserviceable: false,
+    veg: false,
+  },
+  {
+    id: 2,
+    name: "The Mom's Kitchen",
+    cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+    costForTwo: 40000,
+    costForTwoString: "₹400 FOR TWO",
+    cloudinaryImageId: "aizb6uyem2jb1hcrhxac",
+    deliveryTime: 36,
+    avgRating: 4.4,
+    minDeliveryTime: 36,
+    maxDeliveryTime: 36,
+    deliveryTime: "36 MINS",
+    lastMileTravel: 3.5,
+    address: "KFC restaurants, 942,SV Tower, 16th Main, BTM 2nd Stage ,560076",
+    locality: "2nd Stage",
+    parentId: 547,
+    unserviceable: false,
+    veg: false,
+  },
+  {
+    id: 3,
+    name: "Varsha Anand",
+    cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+    costForTwo: 40000,
+    costForTwoString: "₹400 FOR TWO",
+    cloudinaryImageId: "a2ec5418246b622ec4baa3bd051ef6ab",
+    deliveryTime: 36,
+    avgRating: 4.4,
+    minDeliveryTime: 36,
+    maxDeliveryTime: 36,
+    deliveryTime: "36 MINS",
+    lastMileTravel: 3.5,
+    address: "KFC restaurants, 942,SV Tower, 16th Main, BTM 2nd Stage ,560076",
+    locality: "2nd Stage",
+    parentId: 547,
+    unserviceable: false,
+    veg: false,
+  },
+  {
+    id: 4,
+    name: "City Ghar :(",
+    cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+    costForTwo: 40000,
+    costForTwoString: "₹400 FOR TWO",
+    cloudinaryImageId: "rmnzl74nmlldrsv9bnvk",
+    deliveryTime: 36,
+    avgRating: 4.4,
+    minDeliveryTime: 36,
+    maxDeliveryTime: 36,
+    deliveryTime: "36 MINS",
+    lastMileTravel: 3.5,
+    address: "KFC restaurants, 942,SV Tower, 16th Main, BTM 2nd Stage ,560076",
+    locality: "2nd Stage",
+    parentId: 547,
+    unserviceable: false,
+    veg: false,
+  },
+];
 const RestaurantCard = (props) => {
+  const { resData } = props;
+  const {
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+    cloudinaryImageId,
+  } = resData;
+
   return (
     <div className="res-cards" style={styleCard}>
       <img
         className="res-logo"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/c0eeaba9a83d6e5609b6bf24a6b29345"
+        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
       />
-      <h3>{props.resName}</h3>
-      <h4>{props.cuisine}</h4>
-      <h4>4.4 stars</h4>
-      <h4>38 Minutes</h4>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{costForTwo / 100}</h4>
+      <h4>{deliveryTime}</h4>
     </div>
   );
-};
-
-const ResOBJ = {
-  type: "F",
-  id: "334475",
-  name: "KFC",
-  uuid: "eaed0e3b-7c0e-4367-8f59-f41d309fb93a",
-  city: "1",
-  area: "BTM Layout",
-  totalRatingsString: "500+ ratings",
-  cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-  cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-  tags: [],
-  costForTwo: 40000,
-  costForTwoString: "₹400 FOR TWO",
-  deliveryTime: 36,
-  minDeliveryTime: 36,
-  maxDeliveryTime: 36,
-  slaString: "36 MINS",
-  lastMileTravel: 3.5,
-  slugs: {
-    restaurant: "kfc-btm-layout-btm",
-    city: "bangalore",
-  },
-  cityState: "1",
-  address: "KFC restaurants, 942,SV Tower, 16th Main, BTM 2nd Stage ,560076",
-  locality: "2nd Stage",
-  parentId: 547,
-  unserviceable: false,
-  veg: false,
-  select: false,
-  favorite: false,
-  tradeCampaignHeaders: [],
-  aggregatedDiscountInfo: {
-    header: "FREE DELIVERY",
-    shortDescriptionList: [
-      {
-        meta: "FREE DELIVERY",
-        discountType: "FREE_DELIVERY",
-        operationType: "RESTAURANT",
-      },
-    ],
-    descriptionList: [
-      {
-        meta: "FREE DELIVERY",
-        discountType: "FREE_DELIVERY",
-        operationType: "RESTAURANT",
-      },
-    ],
-    subHeader: "",
-    headerType: 0,
-    superFreedel: "",
-  },
-  aggregatedDiscountInfoV2: {
-    header: "",
-    shortDescriptionList: [
-      {
-        meta: "Free Delivery",
-        discountType: "FREE_DELIVERY",
-        operationType: "RESTAURANT",
-      },
-    ],
-    descriptionList: [
-      {
-        meta: "FREE DELIVERY",
-        discountType: "FREE_DELIVERY",
-        operationType: "RESTAURANT",
-      },
-    ],
-    subHeader: "",
-    headerType: 0,
-    superFreedel: "",
-  },
-  ribbon: [
-    {
-      type: "PROMOTED",
-    },
-  ],
-  chain: [],
-  feeDetails: {
-    fees: [],
-    totalFees: 0,
-    message: "",
-    title: "",
-    amount: "",
-    icon: "",
-  },
-  availability: {
-    opened: true,
-    nextOpenMessage: "",
-    nextCloseMessage: "",
-  },
-  longDistanceEnabled: 0,
-  rainMode: "NONE",
-  thirdPartyAddress: false,
-  thirdPartyVendor: "",
-  adTrackingID: "cid=6109309~p=1~eid=00000186-a341-249f-05e6-09c500910178",
-  badges: {
-    imageBased: [],
-    textBased: [],
-    textExtendedBadges: [],
-  },
-  lastMileTravelString: "3.5 kms",
-  hasSurge: false,
-  sla: {
-    restaurantId: "334475",
-    deliveryTime: 36,
-    minDeliveryTime: 36,
-    maxDeliveryTime: 36,
-    lastMileTravel: 3.5,
-    lastMileDistance: 0,
-    serviceability: "SERVICEABLE",
-    rainMode: "NONE",
-    longDistance: "NOT_LONG_DISTANCE",
-    preferentialService: false,
-    iconType: "EMPTY",
-  },
-  promoted: true,
-  avgRating: "3.8",
-  totalRatings: 500,
-  new: false,
 };
 
 const Body = () => {
@@ -167,8 +135,9 @@ const Body = () => {
     <div className="body">
       <div className="search">search</div>
       <div className="res-container">
-        <RestaurantCard resData={ResOBJ} />
-        <RestaurantCard resName="KFC" cuisine="Burger fast food" />
+        {resList.map((list) => (
+          <RestaurantCard key={list.id} resData={list} />
+        ))}
       </div>
     </div>
   );
