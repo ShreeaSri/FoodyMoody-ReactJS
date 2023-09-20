e67dddc0111ae5381c06d171d69cd76346d4bb64
Shreea Sri <shreeasri@gmail.com>
2023-09-20 22:01:40 +0530

    first api call

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,22 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Body = () => {
   const [resNumber, setResNumber] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.093211&lng=80.236398&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const res = await data.json();
+
+    console.log(res);
+  };
 
   return (
     <div className="body">
