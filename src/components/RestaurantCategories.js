33983d3e4772332f42bd21d82b181122213d0c36
Shreea Sri <shreeasri@gmail.com>
2023-09-28 18:28:10 +0530

    build item list display

--- src/components/RestaurantCategories.js
+++ src/components/RestaurantCategories.js
@@ -1,14 +1,19 @@
+import ItemList from "./ItemList";
+
 const RestaurantCategories = ({ data }) => {
   console.log(data);
   return (
     <div>
       {/**header */}
-      <div className="w-6/12 mx-auto my-4 bg-gray shadow p-4 flex justify-between">
-        <span className="text-lg">
-          {`${data.title} (${data.itemCards.length})`}
-        </span>
-        <span>⌄</span>
+      <div className="w-6/12 mx-auto my-4 bg-gray shadow p-4 ">
+        <div className="flex justify-between">
+          <span className="text-lg">
+            {`${data.title} (${data.itemCards.length})`}
+          </span>
+          <span>⌄</span>
+        </div>
         {/**accordian body */}
+        <ItemList items={data.itemCards} />
       </div>
     </div>
   );
