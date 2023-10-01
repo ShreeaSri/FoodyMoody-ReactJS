4d991336101f376ecd8c2f0b136e15e8b09fd5f9
Shreea Sri <shreeasri@gmail.com>
2023-10-01 23:32:50 +0530

    finally completed unit testing for restaurant card

--- src/components/__tests__/RestaurantCard.test.js
+++ src/components/__tests__/RestaurantCard.test.js
@@ -1,20 +1,25 @@
 import { render, screen } from "@testing-library/react";
-import RestaurantCard, { withDiscountLabel } from "../RestaurantCard";
+import RestaurantCard from "../RestaurantCard";
 import MOCK_DATA from "../MockData/resCardMock.json";
+import { withDiscountLabel } from "../RestaurantCard";
 import "@testing-library/jest-dom";
 
 it("should render RestaurantCard component with prop data", () => {
   render(<RestaurantCard resData={MOCK_DATA} />);
 
-  const name = screen.getByText("McDonald's");
+  const name = screen.getByText("Pizza Hut");
 
   expect(name).toBeInTheDocument();
 });
 
-// it("should render the discount restaurant card", () => {
-//   render(withDiscountLabel);
+// will do it later
 
-//   const name = screen.getByText("McDonald's");
+it("should render the discount restaurant card", () => {
+  const RestaurantCardDiscount = withDiscountLabel(RestaurantCard);
 
-//   expect(name).toBeInTheDocument();
-// });
+  render(<RestaurantCardDiscount resData={MOCK_DATA} />);
+
+  const discount = screen.getByText("50% OFF UPTO ₹100");
+
+  expect(discount).toBeTruthy();
+});
